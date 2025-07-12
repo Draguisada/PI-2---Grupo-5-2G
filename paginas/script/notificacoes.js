c4c361c4b28724b3bdde2039bb29260ac8d4848d
Draguisada <draguisada@gmail.com>
2025-07-12 19:31:15 -0300

    Feito sistema de criação de notificação
    por enquanto a adição está apenas em prompt, mas poderá ser melhorado.

--- paginas/script/notificacoes.js
+++ paginas/script/notificacoes.js
@@ -1,13 +1,14 @@
+let idNotificacao = 1
 
 // debug inicio
 const botoesObj = document.getElementsByClassName('botoes')[0];
 
-
 console.log(botoesObj);
 
 botoesObj.addEventListener('click', changeDropdown);
-
 // debug fim
+
+const allNot = document.getElementById('all-notificacoes');
 
 function dropdownButton(event) {
     let pai = event.parentNode;
@@ -49,4 +50,12 @@ function changeDropdown(event) {
     
 }
 
+function deleteNotificacao(e) {
+    e.parentElement.parentElement.remove()
+}
+
+function criarNotificacao() {
+    let texto = prompt('Descrição da notificação');
+    new Notificacao(texto);
+}
 
---
+++ paginas/script/objetos.js
@@ -0,0 +1,43 @@
+class Notificacao {
+    constructor(descricaoNotificacao='nulo') {
+        this.descricaoNotificacao = descricaoNotificacao;
+
+        this.insertNotifc();
+    }
+
+
+    insertNotifc() {
+
+        allNot.innerHTML += `<div class="notificacao">
+
+                <div class="identificacao">
+                    <p>Notificação ${new Date().toLocaleString()}</p>
+                    <p>${idNotificacao++}</p>
+                </div>
+
+                <div class="descricao">
+                    <p>${this.descricaoNotificacao}</p>
+                </div>
+
+                <div class="botoes" id='notListened'>
+                    <button class="dropdown-button" onclick="dropdownButton(this)" title="Ativo">Ativo</button>
+                    <div class="dropdown-conteudo">
+                        <button>Ativo</button>
+                        <button>Feito</button>
+                        <button>Manut.</button>
+                    </div>
+
+                    <button class="lixo" onclick="deleteNotificacao(this)"></button>
+                </div>
+
+        </div>`
+
+
+        let notListened = document.getElementById('notListened');
+
+        notListened.addEventListener('click', changeDropdown);
+
+
+        notListened.id = '';
+    }
+}
