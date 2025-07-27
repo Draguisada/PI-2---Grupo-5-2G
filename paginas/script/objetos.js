5cc1e7b725dc2198014585b21d8f1eca3ae925f0
Draguisada <draguisada@gmail.com>
2025-07-27 19:27:59 -0300

    Feito CSS Notificações

--- paginas/script/objetos.js
+++ paginas/script/objetos.js
@@ -119,16 +119,3 @@ class Notificacao {
 }
 
 
-// Debug
-
-let poste1 = new Poste(1, 1, '1','1', null);
-let poste2 = new Poste(2, 2, '2','2', [poste1]);
-let poste3 = new Poste(3, 3, '3','3', [poste2]);
-let poste4 = new Poste(4, 4, '4','4', [poste3]);
-
-
-poste1.novaNotificacao('ola mundo');
-poste1.novaNotificacao('ola mundo denovo!', 1);
-poste1.novaNotificacao('ola mundo denovo denovo!');
-
-pegarPoste(poste1);
