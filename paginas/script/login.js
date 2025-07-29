ee1ee04011709eda56165784541f7b5355d0319a
Draguisada <draguisada@gmail.com>
2025-07-28 21:07:01 -0300

    Padronizado e Unificado Logins (HTML e CSS)

---
+++ paginas/script/login.js
@@ -0,0 +1,24 @@
+// 0 -> Empresa
+// 1 -> Funcionário
+let estaEm = 0;
+
+
+
+
+function setQuem(status) {
+    estaEm = status;
+
+    if (estaEm) {
+        funcionario();
+    } else {
+        empresa();
+    }
+}
+
+function funcionario() {
+
+}
+
+function funcionario() {
+
+}
